c440e186659efdcde9710f016183a103c69b1853
Umarova Albina <albinaumarova2014@gmail.com>
2025-03-25 16:27:01 +0600

    [API] Добавлена модель для работы с пользователями

---
+++ api/types.d.ts
@@ -0,0 +1,10 @@
+export interface UserFields {
+    email: string;
+    first_name: string;
+    last_name: string;
+    phone_number: string;
+    token: string;
+    role: string;
+    password: string;
+    __confirmPassword: string;
+}
